84c20d4fee06568d6eb4a91f02d3229fbc6fc6a4
mwayush <mwayush@gmail.com>
2021-09-27 18:33:52 +0530

    added isLinkedListLengthEven function

--- linkedList/getMiddleOfList.py
+++ linkedList/getMiddleOfList.py
@@ -13,10 +13,18 @@ def getMiddleOfList(linkedList):
 		if not fast:
 			return slow
 		slow=slow.next
+def isLinkedListLengthEven(linkedList):
+	node=linkedList.head
+	while node:
+		if not node or not node.next:
+			return False
+		node = node.next.next
+	return True
 if __name__=='__main__':
 	myLinkedList=linkedList();
 	myLinkedList.buildLinkedList()
 	print(myLinkedList)
+	print("Given list is Even = "+ str(isLinkedListLengthEven(myLinkedList)))
 	node=getMiddleOfList(myLinkedList)
 	if node:
-		print("middle of given list = " + str(node.data))
+		print("Middle of given list = " + str(node.data))
